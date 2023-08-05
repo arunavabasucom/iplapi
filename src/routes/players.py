355d8c8d0386430c87654ebaf28cc15aa441bf19
Arunava Basu <arunavabasudev@gmail.com>
2023-08-05 12:26:03 +0530

    feat: refactor

--- src/routes/players.py
+++ src/routes/players.py
@@ -7,9 +7,8 @@ router = APIRouter()
 @router.get("/players")
 async def players_data():
     # loading the csv as a dataframe
-    df = pd.read_csv("src/data/players/IPL Player Stat.csv")
+    df = pd.read_csv("src/data/IPL Player Stat.csv")
     # converting json
     json_data = df.to_json(orient="records")
     data = json.loads(json_data)
-
     return data
