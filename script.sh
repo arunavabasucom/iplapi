4229db15173f109a30d16c9382f2ee986caf9eae
Arunava Basu <arunavabasudev@gmail.com>
2023-08-11 13:02:12 +0530

    feat: adding ci

--- script.sh
+++ script.sh
@@ -2,5 +2,7 @@
 pip install -r requirements.txt 
 # start server
 uvicorn src.main:app --host 0.0.0.0 --port 8000
-#
+# for coping ens 
 cp .env.sample .env
+# for test and coverage 
+coverage run --source=src -m pytest -v __tests__/ && coverage report -m
