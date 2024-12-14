f58d7bac3e65e33f9e6ea5900a3e5d6e66b6ac74
Arunava Basu <arunavabasudev@gmail.com>
2024-12-14 20:52:32 +0530

    feat: update script

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ pip install -r requirements.txt
 # start server
 uvicorn src.main:app --host 0.0.0.0 --port 8000
 # for coping ens 
-cp .env.sample .env
+# cp .env.sample .env
 # for test and coverage 
-coverage run --source=src -m pytest -v __tests__/ && coverage report -m
+# coverage run --source=src -m pytest -v __tests__/ && coverage report -m
